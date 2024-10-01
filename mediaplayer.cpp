d5c334faf635416e02010f58b7aa2842a6947055
l1Akr <2539439359@qq.com>
2024-10-01 23:49:40 +0800

    新功能：实现解析一个mp4文件并播放其中的视频部分
            1、申请formatContext
            2、查找对应的流info
            3、定位音视频流索引
            4、查找编解码器并打开
            5、初始化sws图片格式转化器
            6、循环读取packet
            7、解码视频包为frame
            8、转化frame为RGB32格式
            9、提交播放

--- mediaplayer.cpp
+++ mediaplayer.cpp
@@ -5,10 +5,125 @@ MediaPlayer::MediaPlayer(){
 
 }
 
+void MediaPlayer::find_stream_form_context(AVFormatContext* context, int* video_stream , int* audio_stream){
+    if(!context) return;
+    for(unsigned int i = 0; i < context->nb_streams; i++){
+        if(context->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO){
+            *video_stream = i;
+        }else if(context->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO){
+            *audio_stream = i;
+        }
+    }
+    return;
+}
+
 void MediaPlayer::run(){
     qDebug() << __func__ << "\n";
-    m_filePath = "C:/Users/l1Akr/Pictures/微信图片_20240918174554.jpg";
+//    m_filePath = "C:/Users/l1Akr/Pictures/微信图片_20240918174554.jpg";
+    m_filePath = "D:/h.mp4";
     char* path = const_cast<char*>(m_filePath.toStdString().c_str());
-    QImage tmpImage = QImage(path);
-    Q_EMIT SIG_sendImage(tmpImage);
+
+    // 申请formatctf
+    int res = -1;
+    AVFormatContext* formatContext = avformat_alloc_context();
+    res = avformat_open_input(&formatContext, path, NULL, NULL);
+    if(res < 0){
+        qDebug() << "open format context failed\n";
+        return;
+    }
+
+    res = avformat_find_stream_info(formatContext, NULL);
+    if(res < 0){
+        qDebug() << "can not find stream info\n";
+        avformat_close_input(&formatContext);
+        avformat_free_context(formatContext);
+        return;
+    }
+
+    int video_stream_index = -1;
+    int audio_stream_index = -1;
+
+    find_stream_form_context(formatContext, &video_stream_index, &audio_stream_index);
+
+    if(video_stream_index == -1){
+        qDebug() << "video stream index not found\n";
+        avformat_close_input(&formatContext);
+        avformat_free_context(formatContext);
+        return;
+    }
+
+    // 查找视频解码器
+    AVCodecContext* videoCodecCtx = formatContext->streams[video_stream_index]->codec;
+    AVCodec* videoCodec = avcodec_find_decoder(videoCodecCtx->codec_id);
+    if(!videoCodec){
+        qDebug() << "can not find video codec\n";
+        avformat_close_input(&formatContext);
+        avformat_free_context(formatContext);
+        return;
+    }
+
+    // 打开解码器
+    res = avcodec_open2(videoCodecCtx, videoCodec, NULL);
+    if(res < 0){
+        qDebug() << "video codec open failed\n";
+        avformat_close_input(&formatContext);
+        avformat_free_context(formatContext);
+        return;
+    }
+
+    // 开始提取视频包
+
+    AVPacket* packet;
+    AVFrame* frame, *frameRGB;
+    frame = av_frame_alloc();
+    frameRGB = av_frame_alloc();
+    int y_size = videoCodecCtx->width * videoCodecCtx->height;
+    packet = (AVPacket*)malloc(sizeof(AVPacket));
+    av_new_packet(packet, y_size);
+
+    // 初始化用于转换frame为RGB格式的转化器上下文
+    struct SwsContext* image_convert_context;
+    image_convert_context = sws_getContext(videoCodecCtx->width, videoCodecCtx->height, videoCodecCtx->pix_fmt,\
+                                           videoCodecCtx->width, videoCodecCtx->height, AV_PIX_FMT_RGB32, SWS_BICUBIC, NULL, NULL, NULL);
+    int numBytes = avpicture_get_size(AV_PIX_FMT_RGB32, videoCodecCtx->width, videoCodecCtx->height);
+    uint8_t* out_buffer = (uint8_t*)av_malloc(numBytes * sizeof(uint8_t));
+    avpicture_fill((AVPicture*)frameRGB, out_buffer, AV_PIX_FMT_RGB32, videoCodecCtx->width, videoCodecCtx->height);
+
+    // 开始循环获取文件中的packet
+    int ret, got_picture;
+    while(true){
+        if(av_read_frame(formatContext, packet) < 0){
+            break;
+        }
+        if(packet->stream_index == video_stream_index){
+            ret = avcodec_decode_video2(videoCodecCtx, frame, &got_picture, packet);
+            if(ret < 0){
+                qDebug() << "decode video frame error\n";
+                return;
+            }
+            if(got_picture){
+                sws_scale(image_convert_context, (uint8_t const * const *)frame->data,
+                          frame->linesize, 0, videoCodecCtx->height,
+                          frameRGB->data, frameRGB->linesize);
+                QImage tmpImage = QImage((uchar*)out_buffer, videoCodecCtx->width, videoCodecCtx->height, QImage::Format_RGB32);
+                Q_EMIT SIG_sendImage(tmpImage);
+            }
+        }else if(packet->stream_index == audio_stream_index){
+            av_free_packet(packet);
+        }else{
+            av_free_packet(packet);
+        }
+        msleep(5);
+    }
+
+    av_free(frame);
+    av_free(frameRGB);
+    av_free(out_buffer);
+    avcodec_close(videoCodecCtx);
+    avformat_close_input(&formatContext);
+
+
+
 }
+
+
--- mediaplayer.h
+++ mediaplayer.h
@@ -5,6 +5,16 @@
 #include <QImage>
 #include <QString>
 
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libavutil/pixfmt.h"
+#include "libswscale/swscale.h"
+#include "libavformat/avformat.h"
+#include "libavdevice/avdevice.h"
+}
+
 class MediaPlayer : public QThread {
 Q_OBJECT
 signals:
@@ -13,6 +23,9 @@ signals:
 public:
     MediaPlayer();
     void run();
+
+private:
+    void find_stream_form_context(AVFormatContext* context, int* video_stream, int* audio_stream);
 
 private:
     QString m_filePath;
